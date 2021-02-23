d8354fabeac2436f8b22a84cb3a906e2ca7dedab
Vishal Jain <vishal.jain@mirraw.com>
2021-02-23 14:23:12 +0530

    spec changes

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,15 +3,6 @@ require 'rails_helper'
 describe Author do
   subject { create(:author) }
 
-  # describe 'associations' do
-  #   it { should have_many(:books) }
-  # end
-
-  # describe 'validations' do
-  #   it { should validate_presence_of(:first_name) }
-  #   it { should validate_presence_of(:last_name) }
-  # end
-
   it "is valid with valid attributes" do
     expect(subject).to be_valid
   end
