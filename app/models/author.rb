67c90e47086e0447406195eb3e72bc1e521402a2
Vishal Jain <vishal.jain@mirraw.com>
2021-03-02 18:20:06 +0530

    mocks and stubs

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,14 @@ class Author < ApplicationRecord
   has_many :books
 
   validates :first_name, :last_name, presence: true
+
+  def list_book_names
+    self.books.pluck(:title)
+  end
+
+  def name_validate
+    return "Invalid First Name" unless self.valid?
+
+    "#{self.first_name} valid"
+  end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -10,4 +10,17 @@ describe Author do
   it "is invalid attributes" do
     expect(Author.new(last_name: 'abcd')).to_not be_valid
   end
+
+  it "#list_book_names" do
+    expect(subject.list_book_names).to be_an_instance_of(Array)
+  end
+
+  it '#name_validate' do
+    expect(subject.name_validate).to eq("#{subject.first_name} valid")
+  end
+
+  it '#name_validate false' do
+    allow(subject).to receive(:valid?).and_return(false) #stub
+    expect(subject.name_validate).to eq("Invalid First Name")
+  end
 end
